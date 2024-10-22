e14b922486b0e38fa9dc375c596399b58aa58fc9
Sia.Lab <180635855+Sia-Lab-4@users.noreply.github.com>
2024-10-21 20:29:58 -0400

    Week 6

--- selenium_basics/components/base.py
+++ selenium_basics/components/base.py
@@ -1,1 +1,14 @@
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
 
+
+class Base:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def click(self, locator):
+        wait = WebDriverWait(self.driver, 10)
+        elem = wait.until(expected_conditions.element_to_be_clickable(locator))
+        elem.click()
+
+    BASE_VAR = "Base Var"
